eaf181ee35fe428c398f2418228c3ff7f5bad759
shruti-linux <131751004+shruti-linux@users.noreply.github.com>
2025-02-20 20:17:16 +0400

    Update cloud.tf

--- cloud.tf
+++ cloud.tf
@@ -4,7 +4,7 @@ provider "azurerm" {
   subscription_id = "79c65e59-aadb-45ab-9a43-3e10d9e5225e"
 }
 
-resource "azurerm_resource_group" "example143" {
-  name     = "rg-cloud12"
+resource "azurerm_resource_group" "examp12t6" {
+  name     = "rg-cloud1264"
   location = "westus"
 }
